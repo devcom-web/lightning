3bf7a35f6b2376a702d8a84c9ecca5d993a55499
devcom <252250261+devcom-web@users.noreply.github.com>
2026-02-25 18:19:52 -0600

    Create startdef.lua

---
+++ enviro/security/startdef.lua
@@ -0,0 +1,26 @@
+local __py_log = _G.__py_log
+_G.__py_log = nil
+local __py_bit32 = _G.__py_bit32
+_G.__py_bit32 = nil
+local __py_buffer = _G.__py_buffer
+_G.__py_buffer = nil
+--[[ local __EXECUTION_ID = _G.__EXECUTION_ID
+-- _G.__EXECUTION_ID = nil
+_G.__EXECUTION_ID = nil
+local __TRUNCATE_OUTPUT = _G.__TRUNCATE_OUTPUT
+_G.__TRUNCATE_OUTPUT = nil
+local __PLAYER_NAME = _G.__PLAYER_NAME or "TestPlayer"
+_G.__PLAYER_NAME = nil
+local __ENVLOG_CFG = _G.__ENVLOG_CFG or {}
+_G.__ENVLOG_CFG = nil
+local __AUTOPROCESS = _G.__AUTOPROCESS or false
+_G.__AUTOPROCESS = nil ]]
+local python = _G.python
+_G.python = nil
+python = nil
+
+--[[
+      NOTE
+    Anything that's not w/ py in double brackets is from lightning itself for the env. 
+
+--]]
